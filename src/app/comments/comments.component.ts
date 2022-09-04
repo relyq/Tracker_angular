bfec6c90dee229ef36d3f06224b18f0e6fcf6092
relyq <relyqx@gmail.com>
2022-09-04 10:18:03 -0300

    get and post comments working

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CommentService } from '../core/services/comment.service';
+import { Comment } from '../core/models/comment';
 
 @Component({
   selector: 'app-comments',
@@ -7,7 +9,43 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./comments.component.scss']
 })
 export class CommentsComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  comments!: Comment[];
+  comment: Comment = {
+    id: 0,
+    ticketId: Number(this.route.snapshot.paramMap.get('ticketid')),
+    authorId: 'ac5b0b5f-fb48-4cee-b479-b8baf62e8922',
+    content: 'mock comment test',
+    created: new Date()
+  };
 
-  ngOnInit(): void {}
+  constructor(
+    private route: ActivatedRoute,
+    private commentService: CommentService
+  ) {}
+
+  ngOnInit(): void {
+    this.getComments();
+  }
+
+  getComments(): void {
+    const ticketId = Number(this.route.snapshot.paramMap.get('ticketid'));
+    /*
+    this.commentService
+      .getComments(ticketId)
+      .subscribe((comments) => (this.comments = comments));
+  }
+  */
+    this.commentService.getCommentsMock().subscribe((comments) => {
+      this.comments = comments.sort(
+        (a, b) => new Date(b.created).getTime() - new Date(a.created).getTime()
+      );
+    });
+  }
+
+  addComment(): void {
+    console.log(this.comment);
+    this.commentService.postComment(this.comment).subscribe(() => {
+      this.ngOnInit();
+    });
+  }
 }
