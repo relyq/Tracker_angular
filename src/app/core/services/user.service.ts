f24335cacdd4f24694fc5c2d9e53cd12d5cd7161
relyq <relyqx@gmail.com>
2022-09-18 07:36:35 -0300

    service to get users

---
+++ src/app/core/services/user.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private usersUrl = 'https://localhost:7004/api/users';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.usersUrl);
+  }
+
+  getUser(userId: string): Observable<User> {
+    return this.http.get<User>(this.usersUrl + '/' + userId);
+  }
+}
