85fb1f4593d99d18ac3c298f54d9bc95c70dea40
relyq <relyqx@gmail.com>
2022-09-28 08:16:16 -0300

    search & view user details

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../core/services/auth.service';
+import { UserService } from '../core/services/user.service';
+import { User } from '../core/models/user';
 
 @Component({
   selector: 'app-admin',
@@ -8,10 +10,42 @@ import { AuthService } from '../core/services/auth.service';
 })
 export class AdminComponent implements OnInit {
   isAdmin: boolean = false;
+  user!: User;
+  users!: User[];
+  filteredUsers!: User[];
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     this.isAdmin = this.authService.isRole('Administrator');
+    this.getUsers();
+    this.userService
+      .getUser('ac5b0b5f-fb48-4cee-b479-b8baf62e8922')
+      .subscribe((u) => {
+        this.user = u;
+      });
+  }
+
+  getUsers(): void {
+    this.userService.getUsers().subscribe((res) => {
+      this.users = res;
+    });
+  }
+
+  displayFn(user: User) {
+    return user?.username as string;
+  }
+
+  filter(username: string): void {
+    if (typeof username == 'string') {
+      const filterUsername = username.toLowerCase();
+
+      this.filteredUsers = this.users.filter((u) =>
+        u.username.toLowerCase().includes(filterUsername)
+      );
+    }
   }
 }
