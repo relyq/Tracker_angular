d15d48e1a19730288f866bbb6c38113bc7d3c729
relyq <relyqx@gmail.com>
2022-10-16 17:35:36 -0300

    urlify

--- src/app/shared/components/globals.ts
+++ src/app/shared/components/globals.ts
@@ -3,11 +3,16 @@ import { MatTableDataSource } from '@angular/material/table';
 export const baseUrl: string = 'https://localhost:7004';
 
 export function keydown(event: Event, onSubmit: Function): void {
+  const ctrlInputs = [
+    'description',
+    'content',
+    'assignee',
+    'organization',
+    'role'
+  ];
+
   if ((event as KeyboardEvent).key === 'Enter') {
-    if (
-      (event.target as HTMLTextAreaElement).name != 'description' &&
-      (event.target as HTMLTextAreaElement).name != 'content'
-    ) {
+    if (!ctrlInputs.includes((event.target as HTMLTextAreaElement).name)) {
       onSubmit();
     }
     if ((event as KeyboardEvent).ctrlKey) {
@@ -22,4 +27,17 @@ export function searchFilter(
 ): void {
   const filterValue = (event.target as HTMLInputElement).value;
   dataSource.filter = filterValue.trim().toLowerCase();
+}
+
+// this is meant to be used with innerHTML so i'll just \n to <br>
+export function urlify(str: string): string {
+  let rgx = new RegExp(
+    /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/
+  );
+
+  str = str.replace(rgx, (rgx) => `<a href="${rgx}">${rgx}</a>`);
+
+  str = str.replace(new RegExp(/(?:\r\n|\r|\n)/g), '<br>');
+
+  return str;
 }
