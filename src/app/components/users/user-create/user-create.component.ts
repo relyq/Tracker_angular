9672d985f49c35d0792a58db95fde00c2b06fd7a
relyq <nicoale_mainenti@hotmail.es>
2022-11-19 02:52:39 -0300

    user create no longer adds to org

--- src/app/components/users/user-create/user-create.component.ts
+++ src/app/components/users/user-create/user-create.component.ts
@@ -19,19 +19,12 @@ export class UserCreateComponent implements OnInit {
     email: string;
     password: string;
     passwordConfirm: string;
-    organizationId?: string;
-    role?: string;
   } = {
     email: '',
     password: '',
-    passwordConfirm: '',
-    role: ''
+    passwordConfirm: ''
   };
   path: string = '';
-  organizations!: Organization[];
-  filteredOrganizations!: Organization[];
-  roles!: string[];
-  filteredRoles!: string[];
   keydown: Function = keydown;
 
   created: boolean = false;
@@ -39,11 +32,9 @@ export class UserCreateComponent implements OnInit {
   constructor(
     public authService: AuthService,
     private userService: UserService,
-    private organizationService: OrganizationService,
     private router: Router,
     private route: ActivatedRoute,
-    private _ngZone: NgZone,
-    private roleService: RoleService
+    private _ngZone: NgZone
   ) {}
 
   ngOnInit(): void {
@@ -52,24 +43,6 @@ export class UserCreateComponent implements OnInit {
     }
 
     this.isAdmin = this.authService.isRole('Administrator');
-
-    this.getRoles().subscribe((res) => {
-      this.roles = res;
-
-      if (this.path === 'tracker' && this.isAdmin) {
-        this.getOrganizations();
-      }
-    });
-  }
-
-  getRoles(): Observable<string[]> {
-    return this.roleService.getRoles();
-  }
-
-  getOrganizations(): void {
-    this.organizationService.getOrganizations().subscribe((res) => {
-      this.organizations = res;
-    });
   }
 
   createUser = () => {
@@ -78,10 +51,6 @@ export class UserCreateComponent implements OnInit {
       this.userNew.password &&
       this.userNew.password === this.userNew.passwordConfirm
     ) {
-      if (this.path.includes('tracker') && !this.userNew.organizationId) {
-        return;
-      }
-
       this.userService.postUser(this.userNew).subscribe({
         next: (res) => {
           this.created = true;
@@ -94,18 +63,5 @@ export class UserCreateComponent implements OnInit {
   goBack(): void {
     let backRoute = this.path === 'tracker' ? '/tracker/user' : '/admin/user';
     this.router.navigateByUrl(backRoute);
-  }
-
-  displayOrganization(organizationId: string): string {
-    return this.organizations.find((o) => o.id === organizationId)
-      ?.name as string;
-  }
-
-  filterOrganization(organization: string): void {
-    const filterOrganization = organization.toLowerCase();
-
-    this.filteredOrganizations = this.organizations.filter((o) =>
-      o.name.toLowerCase().includes(filterOrganization)
-    );
   }
 }
