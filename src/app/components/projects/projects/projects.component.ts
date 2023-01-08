72419995fdaba7c37833c7c4185476e7535adde0
relyq <relyqx@gmail.com>
2023-01-08 02:48:11 -0300

    projects pagination working

--- src/app/components/projects/projects/projects.component.ts
+++ src/app/components/projects/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Project } from 'src/app/core/models/project';
@@ -21,6 +22,16 @@ export class ProjectsComponent implements OnInit {
   hideDescription: boolean = false;
   hideCreated: boolean = false;
 
+  sort?: string;
+  filterString?: string;
+
+  pageSize: number = 10;
+  totalRows!: number;
+
+  // only show pagination if there are over x results
+  paginationEnabled: boolean = false;
+  readonly paginationRows: number = 10;
+
   filter: Function = searchFilter;
 
   constructor(
@@ -29,9 +40,11 @@ export class ProjectsComponent implements OnInit {
     private breakpointObserver: BreakpointObserver
   ) {}
 
-  @ViewChild(MatSort, { static: false }) set content(sort: MatSort) {
+  @ViewChild(MatSort, { static: false }) set matSort(sort: MatSort) {
     this.dataSource.sort = sort;
   }
+
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
 
   ngOnInit(): void {
     this.breakpointObserver.observe(Breakpoints.XSmall).subscribe((res) => {
@@ -40,18 +53,62 @@ export class ProjectsComponent implements OnInit {
         this.hideDescription = true;
       }
     });
-    this.getProjects();
+    this.getProjects(this.pageSize);
     this.isAdmin = this.authService.isRole('Administrator');
   }
 
-  getProjects(): void {
-    this.projectService.getProjects().subscribe((projects) => {
-      this.projects = projects.sort(
-        (a, b) =>
-          new Date(b.created as Date).getTime() -
-          new Date(a.created as Date).getTime()
-      );
-      this.dataSource.data = this.projects;
-    });
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  getProjects(
+    limit: number,
+    offset?: number,
+    filter?: string,
+    sort?: string
+  ): void {
+    this.dataSource.data = [];
+
+    this.projectService
+      .getProjects(limit, offset, filter, sort)
+      .subscribe((res) => {
+        this.projects = res.projects;
+        this.totalRows = res.count;
+
+        if (this.totalRows > this.paginationRows) {
+          this.paginationEnabled = true;
+        }
+
+        this.projects.sort(
+          (a, b) =>
+            new Date(b.created as Date).getTime() -
+            new Date(a.created as Date).getTime()
+        );
+
+        this.dataSource.data = this.projects;
+      });
+  }
+
+  handlePageEvent(event: PageEvent): void {
+    this.pageSize = event.pageSize;
+    this.getProjects(
+      this.pageSize,
+      event.pageIndex * event.pageSize,
+      this.filterString,
+      this.sort
+    );
+  }
+
+  customSort(e: any) {
+    if (this.paginationEnabled) {
+      this.sort = e.active + '.' + e.direction;
+      this.paginator.firstPage();
+      this.getProjects(this.pageSize, 0, this.filterString, this.sort);
+    }
+  }
+
+  filterAPI(e: Event): void {
+    this.filterString = (e.target as HTMLInputElement).value;
+    this.getProjects(this.pageSize, 0, this.filterString, this.sort);
   }
 }
