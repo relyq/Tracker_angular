720f3a54072c92f57d48b3d0d665d4004ddf7e43
relyq <relyqx@gmail.com>
2023-05-22 02:59:20 -0300

    updated aws baseurl for docker

--- src/app/shared/components/globals.ts
+++ src/app/shared/components/globals.ts
@@ -2,7 +2,8 @@ import { MatTableDataSource } from '@angular/material/table';
 
 export const baseUrl_local: string = 'https://localhost:7004';
 export const baseUrl_pve: string = 'https://relyq.silics.com:7004';
-export const baseUrl_aws: string = 'https://aws-tracker-api.relyq.dev:7004';
+//export const baseUrl_aws: string = 'https://aws-tracker-api.relyq.dev:7004';
+export const baseUrl_aws: string = 'https://aws.relyq.dev:7004';
 //export const baseUrl_vps: string = 'https://vps-2933482-x.dattaweb.com:7004';
 export const baseUrl: string = baseUrl_aws;
 
